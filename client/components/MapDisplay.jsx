4564c553afcb212796445e3a0531ba944c0110cf
Tao Chen <xtc2008@gmail.com>
2022-02-21 18:37:58 -0500

    working map

--- client/components/MapDisplay.jsx
+++ client/components/MapDisplay.jsx
@@ -11,9 +11,18 @@ const MapDisplay = ({google, infoWindow, activeMarker, selectedPlace, markerClic
     height: '100%'
   };
 
+  // const features = eventList.map(el => el = {'position': new google.maps.latLng(eventList.latitude, eventList.longitude)});
+
+//  new google.maps.Marker({
+//   position: new google.maps.LatLng(40.730610, -73.935242)
+//   map: map,
+//   icon: iconBase + 'parking_lot_maps.png'
+// });
+
   const markers = eventList.map((event, i) => {
-    <Marker id={i} event={event} onClick={markerClicker} name={'Kenyatta International Convention Centre'}/>
+    <Marker id={i} event={event} onClick={markerClicker} name={'Insert Label'}/>
   });
+  
 
   return (
     <Map google={google}
@@ -21,11 +30,11 @@ const MapDisplay = ({google, infoWindow, activeMarker, selectedPlace, markerClic
       style={mapStyles}
       initialCenter={
         {
-          lat: 42.730610,
+          lat: 40.730610,
           lng: -73.935242
         }
       } >
-      <Marker onClick={markerClicker} name={'Kenyatta International Convention Centre'}/>
+      <Marker onClick={markerClicker} name={'Insert Label'}/>
       <InfoWindow marker={activeMarker} visible={infoWindow} onClose={closeWindow}>
         <div>
           <h4>{selectedPlace.name}</h4>
